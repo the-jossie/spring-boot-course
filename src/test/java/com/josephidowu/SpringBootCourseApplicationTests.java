4feef1b67e9eeb6f90f495047a3a23f97e28a40b
Dev Jo <idowuayanfeoluwa@gmail.com>
2025-07-23 20:02:20 +0100

    init

---
+++ src/test/java/com/josephidowu/SpringBootCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.josephidowu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCourseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
