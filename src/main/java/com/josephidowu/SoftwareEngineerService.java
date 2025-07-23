d0fd0b37902b446be4f1d7ffab6c36b390baddb6
Dev Jo <idowuayanfeoluwa@gmail.com>
2025-07-24 00:31:26 +0100

    feat: get endpoint

---
+++ src/main/java/com/josephidowu/SoftwareEngineerService.java
@@ -0,0 +1,18 @@
+package com.josephidowu;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class SoftwareEngineerService {
+    private final SoftwareEngineerRepository softwareEngineerRepository;
+
+    public SoftwareEngineerService(SoftwareEngineerRepository softwareEngineerRepository) {
+        this.softwareEngineerRepository = softwareEngineerRepository;
+    }
+
+    public List<SoftwareEngineer> getAllSoftwareEngineers() {
+        return  softwareEngineerRepository.findAll();
+    }
+}
