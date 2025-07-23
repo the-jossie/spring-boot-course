87dae9c27ce1061271a97b19af17b4455cb091f6
Dev Jo <idowuayanfeoluwa@gmail.com>
2025-07-23 21:05:05 +0100

    feat: software engineer controller

---
+++ src/main/java/com/josephidowu/SoftwareEngineerController.java
@@ -0,0 +1,20 @@
+package com.josephidowu;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/software-engineers")
+public class SoftwareEngineerController {
+
+    @GetMapping
+    public List<SoftwareEngineer> getEngineers() {
+        return List.of(
+                new SoftwareEngineer(1, "James", "js, node, react, tailwindcss"),
+                new SoftwareEngineer(1, "Jamila", "java, spring, spring boot")
+        );
+    };
+}
